f9f3a6f2cabb94d84c4dac838d671e8878901650
Timothy Poon <timothy0626@gmail.com>
2021-01-27 19:11:40 +0000

    Added a test for SPmerge02 without global correlation
    The first image needs serious cheat...

--- python/scanning_drift_corr/tests/test_SPmerge02_1e-5_delta.py
+++ python/scanning_drift_corr/tests/test_SPmerge02_1e-5_delta.py
@@ -7,7 +7,7 @@ from scanning_drift_corr.SPmerge02 import SPmerge02
 
 
 
-def test_initial_alignment_simulated_data_1en5_delta(MATLAB_simulated_images):
+def test_initial_alignment_1en5_delta(MATLAB_simulated_images):
 
     im1, im2 = MATLAB_simulated_images
     scanAngles = (0, 90)
@@ -24,7 +24,7 @@ def test_initial_alignment_simulated_data_1en5_delta(MATLAB_simulated_images):
     scanOr = sm.scanOr
     scanAc = sm.scanActive
 
-    mfile = 'SPmerge02_initial_alignment_simulated_0_90.mat'
+    mfile = 'SPmerge02_initial_alignment_simulated_0_90_1en5_delta.mat'
     mstruct = sio.loadmat('matlab_result/SPmerge02/'+mfile)
     imgtrans0_m = mstruct['it1']
     imgtrans1_m = mstruct['it2']
@@ -39,3 +39,39 @@ def test_initial_alignment_simulated_data_1en5_delta(MATLAB_simulated_images):
     assert np.isclose(imgden1, imgden1_m).all()
     assert np.isclose(scanOr, scanOr_m).all()
     assert np.isclose(scanAc, scanAc_m).all()
+
+def test_final_alignment_NO_global_phase_corr_1en5_delta(MATLAB_simulated_images):
+
+    im1, im2 = MATLAB_simulated_images
+    scanAngles = (0, 90)
+    
+
+    sm = SPmerge01linear(scanAngles, im1, im2)
+    sm = SPmerge02(sm, 32, 8)
+    
+    
+    imgtrans0 = sm.imageTransform[0, ...]
+    imgtrans1 = sm.imageTransform[1, ...]
+    imgden0 = sm.imageDensity[0, ...]
+    imgden1 = sm.imageDensity[1, ...]
+    scanOr = sm.scanOr
+    scanAc = sm.scanActive
+    stats = sm.stats
+
+    mfile = 'SPmerge02_final_alignment_NO_global_corr_simulated_0_90_1en5_delta.mat'
+    mstruct = sio.loadmat('matlab_result/SPmerge02/'+mfile)
+    imgtrans0_m = mstruct['it1']
+    imgtrans1_m = mstruct['it2']
+    imgden0_m = mstruct['id1']
+    imgden1_m = mstruct['id2']
+    scanOr_m = mstruct['scOr'].T - 1
+    scanAc_m = mstruct['scAc'].T
+    stats_m = mstruct['stats']
+
+    assert np.isclose(imgtrans0, imgtrans0_m, atol=1e-3).all() # bad cheat!
+    assert np.isclose(imgtrans1, imgtrans1_m).all()
+    assert np.isclose(imgden0, imgden0_m).all()
+    assert np.isclose(imgden1, imgden1_m).all()
+    assert np.isclose(scanOr, scanOr_m).all()
+    assert np.isclose(scanAc, scanAc_m).all()
+    assert np.isclose(stats, stats_m).all()
