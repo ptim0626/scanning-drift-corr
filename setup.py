39e95c65b8e983e6054c2e9122ae7b03dcd171b9
Timothy Poon <timothy0626@gmail.com>
2021-05-28 12:42:53 +0100

    Added setup.cfg pyproject.toml etc

--- setup.py
+++ setup.py
@@ -5,17 +5,22 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
 name="scanning_drift_corr",
-version="0.0.1",
+version="1.0.0",
 author="Timothy Poon",
 author_email="timothy0626@gmail.com",
 description="Correct scan drift distortion",
 long_description=long_description,
 long_description_content_type="text/markdown",
-packages=setuptools.find_packages(),
+url="https://github.com/ptim0626/scanning-drift-corr",
+project_urls={
+    "Bug Tracker": "https://github.com/ptim0626/scanning-drift-corr/issues",
+    },
+package_dir={"": "src"},
+packages=setuptools.find_packages(where="src"),
 classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
 ],
-python_requires='>=3.8',
+python_requires='>=3.7',
 )
