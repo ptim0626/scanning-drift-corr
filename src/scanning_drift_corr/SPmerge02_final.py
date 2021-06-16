a49df0824cf3197c8e074500e5d732168a18e006
Timothy Poon <timothy0626@gmail.com>
2021-06-16 01:46:52 +0100

    Used shared_memory for sharing variable in 02final
    Manager() is slower.

--- src/scanning_drift_corr/SPmerge02_final.py
+++ src/scanning_drift_corr/SPmerge02_final.py
@@ -2,7 +2,7 @@
 """
 
 import warnings
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, shared_memory
 
 import numpy as np
 
@@ -65,7 +65,6 @@ def SPmerge02_final(sm, scanOrStep, **kwargs):
     # Reset pixels moved count
     pixelsMoved = 0
 
-    dxy = np.array([[0,1,-1,0,0], [0,0,0,1,-1]])
     # Refine each image in turn, against the sum of all other images
     for k in range(sm.numImages):
         # get alignment image for the current image
@@ -73,28 +72,49 @@ def SPmerge02_final(sm, scanOrStep, **kwargs):
         imgAgn_sz = imageAlign.shape
         imageAlign = imageAlign.ravel()
 
-        # If ordering is used as a condition, determine parametric positions
-        if flagPointOrder:
-            # Use vector perpendicular to scan direction (negative 90 deg)
-            nn = np.array([sm.scanDir[k, 1], -sm.scanDir[k, 0]])
-            vParam = nn[0]*sm.scanOr[k, 0, :] + nn[1]*sm.scanOr[k, 1, :]
-        else:
-            nn = None
-            vParam = None
-
         if parallel:
+            # set shared memory, used by different processes
+            Z = _set_shm_instance(sm, scanOrStep, imageAlign)
+            shm_scanOr = Z[0]
+            shm_scanLines = Z[1]
+            shm_scanOrStep = Z[2]
+            shm_scanDir = Z[3]
+            shm_imageAlign = Z[4]
+
             # the tasks include all scanline in the current image
             tasks = []
             for m in range(sm.nr):
-                tasks.append([k, m])
+                tasks.append([k, m, sm.nr, sm.nc, sm.numImages, sm.imageSize,
+                              imgAgn_sz, flagPointOrder, shm_scanOr,
+                              shm_scanLines, shm_scanOrStep, shm_scanDir,
+                              shm_imageAlign,
+                              ])
 
-            # set global variables, act like shared data
-            _set_global_objs(sm, scanOrStep, flagPointOrder, dxy, nn, vParam,
-                             imageAlign, imgAgn_sz)
+            pixelsMoved = _parallel_align(tasks, sm, scanOrStep, stepSizeReduce,
+                                          pixelsMoved)
 
-            pixelsMoved = _parallel_align(tasks, sm, scanOrStep,
-                                          stepSizeReduce, pixelsMoved)
+            # clean up the shared memory blocks
+            shm_scanOr.close()
+            shm_scanLines.close()
+            shm_scanOrStep.close()
+            shm_scanDir.close()
+            shm_imageAlign.close()
+            shm_scanOr.unlink()
+            shm_scanLines.unlink()
+            shm_scanOrStep.unlink()
+            shm_scanDir.unlink()
+            shm_imageAlign.unlink()
         else:
+            dxy = np.array([[0,1,-1,0,0], [0,0,0,1,-1]])
+            # If ordering is used as a condition, determine parametric positions
+            if flagPointOrder:
+                # Use vector perpendicular to scan direction (negative 90 deg)
+                nn = np.array([sm.scanDir[k, 1], -sm.scanDir[k, 0]])
+                vParam = nn[0]*sm.scanOr[k, 0, :] + nn[1]*sm.scanOr[k, 1, :]
+            else:
+                nn = None
+                vParam = None
+
             # perform serial alignmment
             pixelsMoved = _serial_align(sm, scanOrStep, k, flagPointOrder,
                                         dxy, nn, vParam, imageAlign,
@@ -108,42 +128,100 @@ def SPmerge02_final(sm, scanOrStep, **kwargs):
 
     return stopRefine
 
-def _set_global_objs(sm, scanOrStep, flagPointOrder, dxy, nn, vParam,
-                     imageAlign, imgAgn_sz):
-    """like shared memory
+def _set_shm_instance(sm, scanOrStep, imageAlign):
+    """set shared memory block for different arrays that used by all processes
     """
-    global Gsm, GscanOrStep, GflagPointOrder, Gdxy, Gnn, GvParam, GimageAlign
-    global GimgAgn_sz
 
-    Gsm = sm
-    GscanOrStep = scanOrStep
-    GflagPointOrder = flagPointOrder
-    Gdxy = dxy
-    Gnn = nn
-    GvParam = vParam
-    GimageAlign = imageAlign
-    GimgAgn_sz = imgAgn_sz
+    # scanOr
+    shm_scanOr = shared_memory.SharedMemory(create=True,
+                                            size=sm.scanOr.nbytes)
+    tmp = np.ndarray(sm.scanOr.shape, dtype=sm.scanOr.dtype,
+                     buffer=shm_scanOr.buf)
+    tmp[:] = sm.scanOr[:]
 
-    return
+    # scanLines
+    shm_scanLines = shared_memory.SharedMemory(create=True,
+                                               size=sm.scanLines.nbytes)
+    tmp = np.ndarray(sm.scanLines.shape, dtype=sm.scanLines.dtype,
+                     buffer=shm_scanLines.buf)
+    tmp[:] = sm.scanLines[:]
+
+    # scanOrStep
+    shm_scanOrStep = shared_memory.SharedMemory(create=True,
+                                                size=scanOrStep.nbytes)
+    tmp = np.ndarray(scanOrStep.shape, dtype=scanOrStep.dtype,
+                     buffer=shm_scanOrStep.buf)
+    tmp[:] = scanOrStep[:]
+
+    # scanDir
+    shm_scanDir = shared_memory.SharedMemory(create=True,
+                                             size=sm.scanDir.nbytes)
+    tmp = np.ndarray(sm.scanDir.shape, dtype=sm.scanDir.dtype,
+                     buffer=shm_scanDir.buf)
+    tmp[:] = sm.scanDir[:]
+
+    # imageAlign
+    shm_imageAlign = shared_memory.SharedMemory(create=True,
+                                                size=imageAlign.nbytes)
+    tmp = np.ndarray(imageAlign.shape, dtype=imageAlign.dtype,
+                     buffer=shm_imageAlign.buf)
+    tmp[:] = imageAlign[:]
+
+    return (shm_scanOr, shm_scanLines, shm_scanOrStep, shm_scanDir,
+            shm_imageAlign)
 
 def _do_align(task):
     """perform alginment for a specific scan m in image k
     """
 
-    k, m = task
+    # unpack variables in the provided order
+    k = task[0]
+    m = task[1]
+    nr = task[2]
+    nc = task[3]
+    numImage = task[4]
+    imageSize = task[5]
+    imgAgn_sz = task[6]
+    flagPointOrder = task[7]
+    shm_scanOr = task[8]
+    shm_scanLines = task[9]
+    shm_scanOrStep = task[10]
+    shm_scanDir = task[11]
+    shm_imageAlign = task[12]
+
+    # create numpy array from the shared memory blocks
+    scanOr = np.ndarray((numImage, 2, nr), dtype=float, buffer=shm_scanOr.buf)
+    scanLines = np.ndarray((numImage, nr, nc), dtype=float,
+                           buffer=shm_scanLines.buf)
+    scanOrStep = np.ndarray((numImage, nr), dtype=float,
+                            buffer=shm_scanOrStep.buf)
+    scanDir = np.ndarray((numImage, 2), dtype=float, buffer=shm_scanDir.buf)
+    imageAlign = np.ndarray((imgAgn_sz[0]*imgAgn_sz[1],), dtype=float,
+                            buffer=shm_imageAlign.buf)
+
+    dxy = np.array([[0,1,-1,0,0], [0,0,0,1,-1]])
+    # If ordering is used as a condition, determine parametric positions
+    if flagPointOrder:
+        # Use vector perpendicular to scan direction (negative 90 deg)
+        nn = np.array([scanDir[k, 1], -scanDir[k, 0]])
+        vParam = nn[0]*scanOr[k, 0, :] + nn[1]*scanOr[k, 1, :]
+    else:
+        nn = None
+        vParam = None
 
     # Refine score by moving the origin (by dxy*step) of this scanline
     # If required, force ordering of points
-    origin = Gsm.scanOr[k, :, m]
-    step = GscanOrStep[k, m]
-    orTest = _origin_ordering(Gsm, origin, m, Gdxy, step, Gnn, GvParam,
-                              GflagPointOrder)
+    origin = scanOr[k, :, m]
+    step = scanOrStep[k, m]
+    orTest = _origin_ordering(nr, origin, m, dxy, step, nn, vParam,
+                              flagPointOrder)
 
     # Loop through the test origins
     # score each of them against the reference image (imageAlign)
-    raw_scanline = Gsm.scanLines[k, m, :]
-    ind, norigin = _test_origins(Gsm, orTest, k, Gdxy, GimageAlign,
-                                 GimgAgn_sz, raw_scanline)
+    raw_scanline = scanLines[k, m, :]
+    ind, norigin = _test_origins(nc, scanDir, imageSize, orTest, k,
+                                 dxy, imageAlign,
+                                 imgAgn_sz, raw_scanline)
 
     return ind, norigin, k, m
 
@@ -154,7 +232,7 @@ def _parallel_align(tasks, sm, scanOrStep, stepSizeReduce, pixelsMoved):
     chsz = sm.nr // cpu_count() + 1
     with Pool(cpu_count()) as pool:
         for ret in pool.imap_unordered(_do_align, tasks, chunksize=chsz):
-            ind, norigin, k, m  = ret
+            ind, norigin, k, m = ret
 
             # record the pixel shift
             pixelsMoved = _move_origin(sm, k, m, scanOrStep, stepSizeReduce,
@@ -173,14 +251,15 @@ def _serial_align(sm, scanOrStep, k, flagPointOrder, dxy, nn, vParam,
         # If required, force ordering of points
         origin = sm.scanOr[k, :, m]
         step = scanOrStep[k, m]
-        orTest = _origin_ordering(sm, origin, m, dxy, step, nn, vParam,
+        orTest = _origin_ordering(sm.nr, origin, m, dxy, step, nn, vParam,
                                   flagPointOrder)
 
         # Loop through the test origins
         # score each of them against the reference image (imageAlign)
         raw_scanline = sm.scanLines[k, m, :]
-        ind, norigin = _test_origins(sm, orTest, k, dxy,
-                                    imageAlign, imgAgn_sz, raw_scanline)
+        ind, norigin = _test_origins(sm.nc, sm.scanDir, sm.imageSize, orTest,
+                                     k, dxy, imageAlign, imgAgn_sz,
+                                     raw_scanline)
 
         # record the pixel shift
         pixelsMoved = _move_origin(sm, k, m, scanOrStep, stepSizeReduce,
@@ -208,7 +287,7 @@ def _get_reference_image(sm, k, densityCutoff):
 
     return imageAlign
 
-def _origin_ordering(sm, origin, IndOr, dxy, step, nn, vParam, flagPointOrder):
+def _origin_ordering(nr, origin, IndOr, dxy, step, nn, vParam, flagPointOrder):
     """Get the moved origin from dxy and step, order them if required
     """
 
@@ -219,7 +298,7 @@ def _origin_ordering(sm, origin, IndOr, dxy, step, nn, vParam, flagPointOrder):
         if IndOr == 0:
             # no lower bound?
             vBound = np.array([-np.inf, vParam[1]])
-        elif IndOr == sm.nr-1:
+        elif IndOr == nr-1:
             # no upper bound?
             vBound = np.array([vParam[IndOr-1], np.inf])
         else:
@@ -236,7 +315,8 @@ def _origin_ordering(sm, origin, IndOr, dxy, step, nn, vParam, flagPointOrder):
 
     return moved_origin
 
-def _test_origins(sm, orTest, k, dxy, imageAlign, imgAgn_sz, raw_scanline):
+def _test_origins(nc, scanDir, imageSize, orTest, k, dxy, imageAlign,
+                  imgAgn_sz, raw_scanline):
     """Loop through the test origins, score each of them against the
     reference image (imageAlign)
 
@@ -246,8 +326,8 @@ def _test_origins(sm, orTest, k, dxy, imageAlign, imgAgn_sz, raw_scanline):
     score = np.zeros(dxy.shape[1])
     for p in range(dxy.shape[1]):
         torigin = orTest[:, p]
-        score[p] = _get_test_origin_score(sm, imageAlign, imgAgn_sz,
-                                          torigin, k, raw_scanline)
+        score[p] = _get_test_origin_score(nc, scanDir, imageSize, imageAlign,
+                                          imgAgn_sz, torigin, k, raw_scanline)
 
     # Note that if moving origin does not change score, dxy = (0,0)
     # will be selected (ind = 0).
@@ -272,20 +352,21 @@ def _move_origin(sm, k, m, scanOrStep, stepSizeReduce, ind, norigin,
 
     return pixelsMoved
 
-def _get_test_origin_score(sm, imageRef, imgRef_sz, torigin, IndImg, scanline):
+def _get_test_origin_score(nc, scanDir, imageSize, imageRef, imgRef_sz,
+                           torigin, IndImg, scanline):
     """Get interpolated scanline from moved origins and compare its with
     raw scanline for scoring
     """
 
     xInd, yInd = torigin
 
-    t = np.arange(1, sm.nc+1)
-    xInd = xInd + t*sm.scanDir[IndImg, 0]
-    yInd = yInd + t*sm.scanDir[IndImg, 1]
+    t = np.arange(1, nc+1)
+    xInd = xInd + t*scanDir[IndImg, 0]
+    yInd = yInd + t*scanDir[IndImg, 1]
 
     # Prevent pixels from leaving image boundaries
-    xInd = np.core.umath.clip(xInd, 0, sm.imageSize[0]-2).ravel()
-    yInd = np.core.umath.clip(yInd, 0, sm.imageSize[1]-2).ravel()
+    xInd = np.core.umath.clip(xInd, 0, imageSize[0]-2).ravel()
+    yInd = np.core.umath.clip(yInd, 0, imageSize[1]-2).ravel()
 
     # Bilinear coordinates
     xF = np.floor(xInd).astype(int)
